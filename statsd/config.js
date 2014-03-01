a121dff08414bd170cb084b2c414dc9180cb6cb4
Joshua Conner <joshua.conner@adcolony.com>
2014-02-28 17:07:19 -0800

    try to get get statsd config values from env before falling back to defaults

--- statsd/config.js
+++ statsd/config.js
@@ -1,6 +1,7 @@
+process = require
 {
-  graphitePort: 2003,
-  graphiteHost: "graphite",
-  port: 8125,
+  graphitePort: process.env.GRAPHITE_PORT || 2003,
+  graphiteHost: process.env.GRAPHITE_HOST || "graphite",
+  port: process.env.PORT || 8125,
   backends: ["./backends/graphite"]
 }
